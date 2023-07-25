867e381087022645b77151dcbd75d27707bfc358
Emerson Silva <emerson_jds@live.com>
2023-07-24 22:36:12 -0300

    start project realtr

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import Home from './src/pages/Home';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const AwesomeProject: React.FC = () => {
+  return (
+    <Home />
+  );
+}
+
+export default AwesomeProject;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const Home: React.FC = () => {
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        setTitle('Teste v72.0 wiht NativeWind');
+    }, []);
+
+    return (
+        <>
+            <View className="flex-1  justify-center items-center">
+                <Text className="text-2xl text-black">
+                    {title}
+                </Text>
+            </View>
+        </>
+    );
+}
+
+export default Home;
